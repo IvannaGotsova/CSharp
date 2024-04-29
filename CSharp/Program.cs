c5e46c8e7bdc951a0c90dd94f266df519c2d2148
IvannaGotsova <ivannagotsova@outlook.com>
2024-04-29 16:41:13 +0300

    Created instance of Datetimes and Strings

--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -1,10 +1,21 @@
 ﻿namespace CSharp
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            
+            Console.WriteLine("-----------------------------------------------------------");
+            Strings strings = new Strings();
+            strings.PrintStrings();
+            strings.ManipulationStringArrays();
+            strings.FormatStrings();
+            
+            Console.WriteLine("-----------------------------------------------------------");
+            DateTimes dateTimes = new DateTimes();
+            dateTimes.PrintDateTimes();
+            dateTimes.FormatDateTimes();
         }
     }
 }
