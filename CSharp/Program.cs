1a9501ee6847701794b7d06050c9cebac17e387f
IvannaGotsova <ivannagotsova@outlook.com>
2024-08-31 20:21:04 +0300

    Multidimensiona array

--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -67,7 +67,15 @@ namespace CSharp
             //StatusPerson statusPerson = StatusPerson.Student;
             //Console.WriteLine(statusPerson);
 
-            
+            int[,] multiArray = { { 1, 2, 3, }, { 4, 5, 6 }, { 7, 8, 9 } };
+
+            for (int i = 0; i < multiArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < multiArray.GetLength(1); j++)
+                {
+                    Console.WriteLine(multiArray[i,j]);
+            }
+            }
 
 
         }
