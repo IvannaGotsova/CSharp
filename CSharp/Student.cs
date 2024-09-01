96869d86f6b4bc95d48dbc4750ba7b1f5f1f869a
IvannaGotsova <ivannagotsova@outlook.com>
2024-09-01 13:58:29 +0300

    class Student with constructor and override method ToStribng()

--- CSharp/Student.cs
+++ CSharp/Student.cs
@@ -11,5 +11,10 @@ namespace CSharp
         public Student(int id, string name, int age) : base(id, name, age)
         {
         }
+
+        public override string ToString()
+        {
+            return "I am Director";
+        }
     }
 }
