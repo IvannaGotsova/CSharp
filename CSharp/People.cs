f8678c5d6089fde4a46e798d68f0bc48d3f6ca41
IvannaGotsova <ivannagotsova@outlook.com>
2024-09-01 15:03:17 +0300

    Created abstract class

---
+++ CSharp/People.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp
+{
+    internal abstract class People
+    {
+        public abstract void abstractPeople();
+
+        public void noAbstractPeople()
+        {
+            Console.WriteLine("No abstract method");
+        }
+
+    }
+}
