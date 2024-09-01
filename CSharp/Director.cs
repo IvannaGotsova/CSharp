961628cfcb0056cb5428cf5208cc940994f5b99b
IvannaGotsova <ivannagotsova@outlook.com>
2024-09-01 14:06:53 +0300

    Corrected with constructor and override ToString()

--- CSharp/Director.cs
+++ CSharp/Director.cs
@@ -6,8 +6,15 @@ using System.Threading.Tasks;
 
 namespace CSharp
 {
-    internal class Director
+    internal class Director : Person
     {
+        public Director(int id, string name, int age) : base(id, name, age)
+        {
+        }
 
+        public override string ToString()
+        {
+            return "I am Director";
+        }
     }
 }
