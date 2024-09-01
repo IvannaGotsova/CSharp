a1ce8729211df58eed6ade9cdecd5c91d1c8a69a
IvannaGotsova <ivannagotsova@outlook.com>
2024-09-01 14:07:49 +0300

    Corrected with constructor and override ToString()

--- CSharp/Teacher.cs
+++ CSharp/Teacher.cs
@@ -6,8 +6,15 @@ using System.Threading.Tasks;
 
 namespace CSharp
 {
-    internal class Teacher
+    internal class Teacher : Person
     {
-        
+        public Teacher(int id, string name, int age) : base(id, name, age)
+        {
+        }
+
+        public override string ToString()
+        {
+            return "I am Teacher";
+        }
     }
 }
