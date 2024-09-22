a793857a1de1aaf98a00ac51408dd8b0d2b0467e
IvannaGotsova <ivannagotsova@outlook.com>
2024-09-22 23:48:22 +0300

    Exception.StackTrace

--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -107,7 +107,7 @@ namespace CSharp
             }
             catch (Exception e)
             {
-                Console.WriteLine(e.Message);
+                Console.WriteLine(e.StackTrace);
             }
             finally
             {
