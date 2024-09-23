6ea56a5883b53e91504c3a8cf5a31ef621828ad5
IvannaGotsova <ivannagotsova@outlook.com>
2024-09-23 18:29:21 +0300

    Created class for Reflection

---
+++ CSharp/ReflectionClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp
+{
+    public class ReflectionClass
+    {
+        public string ReflectionString;
+        public int ReflectionInt;
+
+        public ReflectionClass(string reflectionString, int reflectionInt)
+        {
+            ReflectionString = reflectionString;
+            ReflectionInt = reflectionInt;
+        }
+
+        public void ReflectionMethosString(string example)
+        {
+            Console.WriteLine(example);
+        }
+
+        public void ReflectionMethosInt(int example)
+        {
+            Console.WriteLine(example);
+        }
+    }
+}
