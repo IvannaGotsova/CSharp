a7b4f5b2fbce77fa8e7a538976888519c66e49a7
IvannaGotsova <ivannagotsova@outlook.com>
2024-09-23 19:44:42 +0300

    Created ExtensionMethods

---
+++ CSharp/ExtensionMethods.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp
+{
+    internal static class PersonExtensions
+    {
+        public static bool IsGradeGood(this Person person, int grade)
+        {
+            return grade >= 100;
+        }
+
+
+    }
+}
